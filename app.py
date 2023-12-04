500b257971bd41cca11afd597219b33c3ffe8ff1
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-12-04 00:00:02 +0000

    add date and time

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 # Basic Website Template
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html', title='Home')
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/')
+def hello():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
