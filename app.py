328d15c4d051b9e667f0fe62107867b311f36985
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-12-07 22:16:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    dt = datetime.datetime.utcnow()
+    return render_template_string(
+        '''
+        <html><body>
+        <h4>UTC --> Local Time</h4>
+        Datetime right now is:
+        <p>{{ dt | datetimefilter }} (utc)</p>
+        <p><span id="timeNow"></span> (local)</p>
+        <script>
+        var elem = document.getElementById("timeNow")
+        var now = new Date();
+        var options = { month: 'short', day: '2-digit',
+                        hour: 'numeric', minute: '2-digit' };
+        elem.innerHTML = now.toLocaleString('en-us', options);
+        </script>
+        </body></html>
+        ''',
+        dt=dt)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
